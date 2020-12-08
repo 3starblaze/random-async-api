88b6d4a74ce77c55c11227b96b87ce1847eb208e
Deniss Fjodorovs <3starblaze@gmail.com>
2020-12-08 13:10:42 +0200

    Define randomRange()

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+// Return a random integer in range [min; max)
+function randomRange(min, max) {
+    const delta = max - min;
+    return Math.random() * delta + min;
+}
+
 app.get('/', function (req, res) {
     res.send('Hello world!')
 });
