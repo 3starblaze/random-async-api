2726f889b1965f382839a9911172bf5fa6987edc
Deniss Fjodorovs <3starblaze@gmail.com>
2020-12-08 13:57:49 +0200

    Add `/number` GET API

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,16 @@ app.get('/', function (req, res) {
     res.send('Hello world!')
 });
 
+app.get('/number', function (req, res) {
+    const delay = Number(req.query.delay) || randomRange(0, 6);
+    const value = Number(req.query.value) || randomRange(0, 1000);
+    setTimeout(() => {
+        res.json({
+            data: value
+        });
+    }, delay * 1000);
+});
+
 app.listen(3000, function() {
     console.log('App listening on port 3000');
 });
