1f8b174b833fa61554a23b0a80952344a6b5e41d
Deniss Fjodorovs <3starblaze@gmail.com>
2020-12-08 14:09:12 +0200

    Create `/string` GET API

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ function randomRange(min, max) {
     return Math.floor(Math.random() * delta + min);
 }
 
+function randomChar() {
+    return String.fromCharCode(randomRange(0x20, 0x7f + 1));
+}
+
 app.get('/', function (req, res) {
     res.send('Hello world!')
 });
@@ -14,6 +18,25 @@ app.get('/', function (req, res) {
 app.get('/number', function (req, res) {
     const delay = Number(req.query.delay) || randomRange(0, 6);
     const value = Number(req.query.value) || randomRange(0, 1000);
+    setTimeout(() => {
+        res.json({
+            data: value
+        });
+    }, delay * 1000);
+});
+
+app.get('/string', function (req, res) {
+    const delay = Number(req.query.delay) || randomRange(0, 6);
+    let value = req.query.value;
+
+    if (!value) {
+        value = '';
+        const length = randomRange(0, 100)
+        for (let i = 0; i < length; i++) {
+            value += randomChar();
+        }
+    }
+
     setTimeout(() => {
         res.json({
             data: value
